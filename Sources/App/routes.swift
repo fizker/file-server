eaf1673ab01864aae8dd280903bef4d24790e4fd
Benjamin Horsleben <benjamin@fizkerinc.dk>
2020-12-02 18:48:00 +0100

    Added basic steps towards receiving data

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,5 +1,10 @@
 import Vapor
 
+struct FileUpload: Decodable {
+	var file: File
+	var foo: String
+}
+
 func routes(_ app: Application) throws {
 	app.get { req in
 		return "It works!"
@@ -7,5 +12,10 @@ func routes(_ app: Application) throws {
 
 	app.get("hello") { req -> String in
 		return "Hello, world!"
+	}
+
+	app.post("file") { req -> String in
+		let dto = try req.content.decode(FileUpload.self)
+		return "\(dto.file.filename): \(dto.file.data.readableBytes)"
 	}
 }
